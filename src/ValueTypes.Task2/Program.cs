0a1a09226bbc9ee7d6a8fc8e3feea7b19360b702
ArtMuxomor <27068331+ArtMuxomor@users.noreply.github.com>
2026-02-27 19:41:53 +0300

    feat: Добавил второе задание ValueTypes

---
+++ src/ValueTypes.Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace ValueTypes.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static IEnumerable<string> BuildDiamond(int n)
+        {
+            if (n % 2 == 0 || n <= 0)
+            {
+                yield break;
+            }
+
+            int center = n / 2;
+            var diamondLine = new StringBuilder(n);
+
+            for (int row = 0; row < n; row++)
+            {
+                int distToCenter = Math.Abs(center - row);
+
+                diamondLine.Clear()
+                    .Append(' ', distToCenter)
+                    .Append('X');
+
+                if (distToCenter < center)
+                {
+                    diamondLine
+                        .Append(' ', n - (distToCenter + 1) * 2)
+                        .Append('X');
+                }
+
+                yield return diamondLine.ToString();
+            }
+        }
+    }
+}

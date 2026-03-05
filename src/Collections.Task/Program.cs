b2ec40551aafed775d47774d797fc14af53979e1
ArtMuxomor <27068331+ArtMuxomor@users.noreply.github.com>
2026-03-05 20:06:01 +0300

    wip: Добавил скелет класса SmartStack в Collections

---
+++ src/Collections.Task/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Collections.Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var stack = new SmartStack<int>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ src/Collections.Task/SmartStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Task
+{
+    class SmartStack<T> : IEnumerable<T>
+    {
+        private T[] _values;
+        private int _count;
+
+        public int Count => throw new NotImplementedException();
+
+        public int Capacity => throw new NotImplementedException();
+
+        public SmartStack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SmartStack(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SmartStack(IEnumerable<T> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushRange(IEnumerable<T> values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
